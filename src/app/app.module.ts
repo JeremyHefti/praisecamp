c1e9d8931d94951703e5ce5894662580755156bf
Jeremy Hefti <jeremy.hefti@raiffeisen.ch>
2024-11-15 13:18:52 +0100

    added card and navigate with navbar, started the info page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {MenubarModule} from "primeng/menubar";
+import { EventComponent } from './event/event.component';
+import { CardComponent } from './card/card.component';
+import {CardModule} from "primeng/card";
+import {Button} from "primeng/button";
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventComponent,
+    CardComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MenubarModule,
+    CardModule,
+    Button,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/info/info.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+  eventDate: string = '27.12.2024 - 01.01.2024';
+  eventLocation: string = 'Messe Basel';
+  eventCost: number = 360;
+
+  members = [
+    { name: 'Jeremy Hefti', role: 'Organisator' },
+    { name: 'Elina Hefti', role: 'Organisator' },
+    { name: 'Leon Höfferer', role: 'Teilnehmer/Dev' },
+    { name: 'Elina', role: 'Teilnehmer' },
+    { name: 'Jana Hefti', role: 'Teilnehmer' },
+    { name: 'Jana Ranieli', role: 'Teilnehmer' },
+    { name: 'Mea Ruprecht', role: 'Teilnehmer' },
+    { name: 'Micha Ruprecht', role: 'Teilnehmer' },
+    { name: 'Seraina', role: 'Teilnehmer' },
+    { name: 'Yöni', role: 'Teilnehmer' },
+
+  ];
+}
